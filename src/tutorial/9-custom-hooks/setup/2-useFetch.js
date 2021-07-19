d76aecdce3be5b7b43137fc8ade54c277c17ab60
Cloud_Forest <61158187+trongtuanit@users.noreply.github.com>
2021-07-20 05:49:41 +0700

    custom hooks

--- src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,3 +1,20 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-export const useFetch = () => {};
+export const useFetch = (url) => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setData(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
+    // eslint-disable-next-line
+  }, [url]);
+
+  return { loading, data };
+};
