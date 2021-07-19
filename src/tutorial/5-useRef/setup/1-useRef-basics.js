718a5e1ed22c1fe4e43156e0878cc9a4b81f9c2b
Cloud_Forest <61158187+trongtuanit@users.noreply.github.com>
2021-07-19 13:43:32 +0700

    useReducer

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,30 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 
 // preserves value
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  const refContainer = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(refContainer.current);
+  };
+
+  useEffect(() => {
+    console.log(refContainer);
+    refContainer.current.focus();
+  });
+
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <input type="text" ref={refContainer} />
+        <button type="submit">submit</button>
+      </form>
+    </>
+  );
 };
 
 export default UseRefBasics;
--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,64 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from "react";
+import Modal from "./Modal";
+import { data } from "../../../data";
 // reducer function
+import { reducer } from "./reducer";
+
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: "hello world",
+};
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [name, setName] = useState("");
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name) {
+      const newPerson = { id: new Date().getTime().toString(), name };
+      dispatch({ type: "ADD_ITEM", payload: newPerson });
+      setName("");
+    } else {
+      dispatch({ type: "NO_VALUE" });
+    }
+  };
+
+  const closeModal = () => {
+    dispatch({ type: "CLOSE_MODAL" });
+  };
+
+  return (
+    <>
+      {state.isModalOpen && (
+        <Modal closeModal={closeModal} modalContent={state.modalContent} />
+      )}
+      <form onSubmit={handleSubmit} className="form">
+        <div>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <button type="submit">Add</button>
+      </form>
+      {state.people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+            <button
+              onClick={() => dispatch({ type: "REMOVE_ITEM", payload: id })}
+            >
+              Remove
+            </button>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Index;
